90226b80b77d59e5c2356c1b2bb3f8020017e37c
Võ Trường Dũng <87188466+mrrobjn@users.noreply.github.com>
2023-10-07 01:21:40 +0700

    fix api

--- src/controllers/weatherforecast.js
+++ src/controllers/weatherforecast.js
@@ -1,13 +1,13 @@
-import { fetchAPI } from '../api/fetchApi.js';
+import { fetchAPI } from "../api/fetchApi.js";
 export const getWeatherForecast = async (req, res) => {
-    
-    const {latitude,longitude,hourly,forecastDays}=req.query
-    // const latitude=52.52;
-    // const longitude=13.41;
-    // const hourly ='pm10,pm2_5';
-    // const forecastDays=7;
-    const apiUrl = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${latitude}&longitude=${longitude}&hourly=${hourly}&timezone=Asia%2FBangkok&forecast_days=${forecastDays}`;
-    const result = await fetchAPI(apiUrl)
+  const { latitude, longitude, hourly, daily } = req.query;
+  let apiUrl = "";
+  if (daily) {
+    apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=${hourly}&daily=${daily}&timezone=Asia%2FBangkok`;
+  } else {
+    apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=${hourly}`;
+  }
+  const result = await fetchAPI(apiUrl);
 
-res.json(result)
-}
+  res.json(result);
+};
