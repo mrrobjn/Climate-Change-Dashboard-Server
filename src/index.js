4b9125467b157274b196a04f98decb56dfea3595
mrrobjn <votruongdung4@gmail.com>
2023-10-24 16:30:00 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import cors from "cors";
 import { route } from "./routers/index.js";
+import { PythonShell } from "python-shell";
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
-
+let options = {
+  mode: "text",
+  scriptPath: "C:/Climate-Change-Dashboard-Server/src/scripts",
+  pythonOptions: ["-u"], // get print results in real-time
+  args: ["value1", "value2", "value3"],
+};
 route(app);
-
+PythonShell.run("test.py", options).then((results) => {
+  // results is an array consisting of messages collected during execution
+  console.log("results: %j", results);
+});
 app.listen(PORT);
