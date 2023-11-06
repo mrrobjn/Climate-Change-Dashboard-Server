5f8ff2c7889500725f38970e19177563797cd9f8
maithanhtoan1289 <maithantoan1070@gmail.com>
2023-11-06 13:05:29 +0700

    delete

--- src/app/models/Articlescontents.js
+++ src/app/models/Articlescontents.js
@@ -1,11 +1,22 @@
-import mongoose from 'mongoose';
-      const ArticleContentSchema = new mongoose.Schema({
-        articles_id: { type: String },
-        type: { type: String, default: 'articles' }, 
-        order:  { type: Number },
-        data:  { type: String },
-      },{
-        timestamps: true,
-      });
-     const ArticleContent = mongoose.model('ArticleContent', ArticleContentSchema, 'articles_contents');
-export default ArticleContent ;
+import mongoose from "mongoose";
+const ArticleContentSchema = new mongoose.Schema(
+  {
+    articles_id: { type: String },
+    type: { type: String, default: 'article' }, 
+    base64: { type: String },
+    question: { type: String },
+    rationale: { type: String },
+    visualization: { type: String },
+    description: { type: String },
+    index: { type: Number },
+  },
+  {
+    timestamps: true,
+  }
+);
+const ArticleContent = mongoose.model(
+  "ArticleContent",
+  ArticleContentSchema,
+  "articles_contents"
+);
+export default ArticleContent;
