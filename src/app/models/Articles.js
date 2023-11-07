df34798d59faff06992724ae577edb0a7868528b
maithanhtoan1289 <maithantoan1070@gmail.com>
2023-11-07 08:29:01 +0700

    Squashed commit of the following:
    commit 97c33cbcc54131c37f38e21f8403a2417c77acec
    Author: Võ Trường Dũng <87188466+mrrobjn@users.noreply.github.com>
    Date:   Tue Nov 7 01:17:49 2023 +0700
    
        fix bug
    
    commit 0a8c26d871a41d21cd27e3aed2148d7c93a6d7a0
    Merge: 3d127d7 25e5f2c
    Author: Võ Trường Dũng <87188466+mrrobjn@users.noreply.github.com>
    Date:   Mon Nov 6 13:36:18 2023 +0700
    
        Merge pull request #11 from mrrobjn/Insert_Delete
    
        Insert delete

--- src/app/models/Articles.js
+++ src/app/models/Articles.js
@@ -1,16 +1,17 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema
-      const ArticleSchema = new Schema({
-       // articles_id:{type: String},
-       type: { type: String, default: 'article' }, 
-        title: { type: String },
-        img_url: { type: String },
-        date_created: { type: Date, default: Date.now },
-        view: { type: Number ,default:0 },
-      //  author_id: { type: String },
-      },{
-        timestamps: true,
-      });
-      const Article = mongoose.model('Article', ArticleSchema, 'articles');
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const ArticleSchema = new Schema(
+  {
+    title: { type: String },
+    desc: { type: String },
+    img_url: { type: String },
+    view: { type: Number, default: 0 },
+    //  author_id: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Article = mongoose.model("Article", ArticleSchema, "articles");
 
 export default Article;
--- src/controllers/ArticlesController.js
+++ src/controllers/ArticlesController.js
@@ -1,12 +1,12 @@
 import { ObjectId } from "mongodb";
 import { connectToDatabase } from "../db/index.js";
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import Article from "../app/models/Articles.js";
 import ArticleContent from "../app/models/Articlescontents.js";
 
 export const getArticles = async (req, res) => {
-  const page = parseInt(req.query.page) || 1; 
+  const page = parseInt(req.query.page) || 1;
   const perPage = 4;
   try {
     const articles = await Article.find()
@@ -17,7 +17,7 @@ export const getArticles = async (req, res) => {
     res.status(200).json(articles);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ 'ERR':error });
+    res.status(500).json({ ERR: error });
   }
 };
 export const getSingleArticle = async (req, res) => {
@@ -35,57 +35,75 @@ export const getArticleDetail = async (req, res) => {
   const client = await connectToDatabase();
   const db = client.db("CCD");
 
-  const detailCollection = db.collection("articles_details");
+  const detailCollection = db.collection("articles_contents");
 
-  const result = await detailCollection.find({ articles_id: new ObjectId(id) }).toArray();
+  const result = await detailCollection
+    .find({
+      article_id: new ObjectId(id),
+    })
+    .toArray();
   res.json(result);
 };
- export async function insert(req,res) {
-const ArticleID = new mongoose.Types.ObjectId();
-const dataToInsert = {
-  articles_id: ArticleID,
-  ...req.body,
-};
-const newArticle = new Article(dataToInsert);
-newArticle.save()
-  .then(() => {
-    const newArticleContent = new ArticleContent(dataToInsert);
-    newArticleContent.save()
-      .then(() => {
-        res.status(200).json({ message: "Create Article and ArticleContent successfully" });
-      })
-      .catch((error) => {
-        console.error( error);
-        res.status(400).json({ error });
+export async function insert(req, res) {
+  const ArticleID = new mongoose.Types.ObjectId();
+  const article = {
+    articles_id: ArticleID,
+    ...req.body,
+  };
+  const { contents } = req.body;
+  const newArticle = new Article(article);
+  newArticle
+    .save()
+    .then((savedArticle) => {
+      contents.map((content, index) => {
+        content.index = index;
+        content.article_id = savedArticle._id; // Set the foreign key here
+        const newArticleContent = new ArticleContent(content);
+        newArticleContent
+          .save()
+          .then(() => {
+            res.status(200);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(400).json({ error });
+          });
+        return;
       });
-  })
-  .catch((error) => {
-    console.error( error);
-    res.status(400).json({ error});
-  });
-
+      res.status(200).json({
+        message: "Create Article successfully",
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).json({ error });
+    });
 }
+
 export async function Delete(req, res) {
   const { id, type } = req.body;
   try {
-    if (type === 'articles') {
-
+    if (type === "articles") {
       const articleDeleteResult = await Article.deleteOne({ _id: id });
-      const articleContentsDeleteResult = await ArticleContent.deleteMany({ articles_id: id });
+      const articleContentsDeleteResult = await ArticleContent.deleteMany({
+        articles_id: id,
+      });
       if (articleDeleteResult.deletedCount > 0) {
-        res.json({ message: 'Deletion of articles completed.' });
+        res.json({ message: "Deletion of articles completed." });
       } else {
-        res.json({ message: 'No articles found for deletion.' });
+        res.json({ message: "No articles found for deletion." });
       }
-    } else if (type === 'article_content') {
-      const articleContentDeleteResult = await ArticleContent.deleteOne({ articles_id: id });
+    } else if (type === "article_content") {
+      const articleContentDeleteResult = await ArticleContent.deleteOne({
+        articles_id: id,
+      });
       if (articleContentDeleteResult.deletedCount > 0) {
-        res.json({ message: 'Deletion of articles_content completed.' });
+        res.json({ message: "Deletion of articles_content completed." });
       } else {
-        res.json({ message: 'No articles_content found for deletion.' });
+        res.json({ message: "No articles_content found for deletion." });
       }
     } else {
-      res.json({ message: 'Invalid type parameter.' });
+      res.json({ message: "Invalid type parameter." });
     }
   } catch (error) {
     console.error(error);
