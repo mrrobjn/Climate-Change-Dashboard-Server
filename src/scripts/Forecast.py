5dbd23fe5ee69faca482c1a562696a59b5b93227
maithanhtoan1289 <maithantoan1070@gmail.com>
2023-12-05 19:45:39 +0700

    fix replace_ update validate download

--- src/scripts/Forecast.py
+++ src/scripts/Forecast.py
@@ -1,11 +1,8 @@
-
+from datetime import datetime
 import pymongo
-from bson import  json_util
-from pprint import pprint
-import matplotlib.pyplot as plt
 import json
 import sys
-
+import matplotlib.pyplot as plt
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/CCD")
 
@@ -13,15 +10,8 @@ mycollection = myclient.CCD["forecast"]
 mycollection.create_index([("location", "2dsphere")])
 latitude = float(sys.argv[1])
 longitude = float(sys.argv[2])
-
 componentH = sys.argv[3]
 componentD = sys.argv[4]
-
-if componentH:
-    componentH = componentH.split(',')
-
-if componentD:
-    componentD = componentD.split(',')
 
 query = {
     "location": {
@@ -36,73 +26,91 @@ query = {
 data = mycollection.find(query)
 result_data = [record for record in data]
 
+if componentH:
+    componentH = componentH.split(',')
+    tmpH = []   
+    time_positionsH = []
+    for record in result_data:
+        if "hourly" in record:
+            tmpH.append(record["hourly"]["time"])
+    
+    if tmpH:
+        for idx, row in enumerate(tmpH[0]):
+            time_positionsH.append(idx)
+
+        components_dataH = {}
+        for c in componentH:
+            components_dataH[c] = [record["hourly"][c][idx] for idx in time_positionsH]
+
+
+if componentD:
+    componentD = componentD.split(',')
+    tmpD = []   
+    time_positionsD = []
+    for record in result_data:
+        if "daily" in record:
+            tmpD.append(record["daily"]["time"])
+    
+    if tmpD:
+        for idx, row in enumerate(tmpD[0]):
+            time_positionsD.append(idx)
+        
+        components_dataD = {}
+        for c in componentD:
+            components_dataD[c] = [record["daily"][c][idx] for idx in time_positionsD]
+
 result = {
     "latitude": latitude,
     "longitude": longitude,
-    "hourly": {"time": []},
-    "daily": {"time": []}
 }
 
-for record in result_data:
-    result["hourly"]["time"].extend(record.get("hourly", {}).get("time", []))
-    result["daily"]["time"].extend(record.get("daily", {}).get("time", []))
+if componentH:
+    result["hourly"] = {
+        "time": [row for row in tmpH[0]],
+        **components_dataH
+    }
 
-    if componentH:
-        for c in componentH:
-            if c:
-                result["hourly"][c] = result["hourly"].get(c, []) + record["hourly"].get(c, [])
+if componentD:
+    result["daily"] = {
+        "time": [row for row in tmpD[0]],
+        **components_dataD
+    }
 
-    if componentD:
-        for c in componentD:
-            if c:
-                result["daily"][c] = result["daily"].get(c, []) + record["daily"].get(c, [])
-
-json_data = json_util.dumps(result)
-
-data = json.loads(json_data, object_hook=json_util.object_hook)
-
-chart_type = sys.argv[5]
-
-# Check if chart_type is valid
 valid_chart_types = ["line", "bar"]
+chart_type = sys.argv[5]
 if chart_type and chart_type not in valid_chart_types:
-    print(f"Invalid chart type. Supported types: {', '.join(valid_chart_types)}.")
+    print(f"Loại biểu đồ không hợp lệ. Loại hỗ trợ: {', '.join(valid_chart_types)}.")
     sys.exit(1)
 
-# Define a dictionary to map chart types to plotting functions
 chart_types_mapping = {
     "line": plt.plot,
     "bar": plt.bar,
 }
 
 plot_function = chart_types_mapping.get(chart_type, plt.plot)
+
 component_unitsH = {c: result_data[0]["hourly_units"][c] for c in componentH}
 component_unitsD = {c: result_data[0]["daily_units"][c] for c in componentD}
-
 plt.figure(figsize=(10, 6))
 plot_data = []
 
-# Plot hourly components
+
 if componentH:
     for component in componentH:
-        plot_function = chart_types_mapping.get(chart_type, plt.plot)
-        plot_function(data["hourly"]["time"], data["hourly"][component], label=f"hourly {component} ({component_unitsH[component]})")
+        plot_function(result["hourly"]["time"], result["hourly"][component], label=f"hourly {component} ({component_unitsH[component]})")
         plot_data.append({
-            "x": [row.strftime("%Y-%m-%d %H:%M:%S") for row in data["hourly"]["time"]],
-            "y": data["hourly"][component],
+            "x": [row.strftime("%Y-%m-%d %H:%M:%S") for row in result["hourly"]["time"]],
+            "y": result["hourly"][component],
             "type": chart_type,
             "name": f"hourly {component} ({component_unitsH[component]})"
         })
 
-# Plot daily components
 if componentD:
     for component in componentD:
-        plot_function = chart_types_mapping.get(chart_type, plt.plot)
-        plot_function(data["daily"]["time"], data["daily"][component], label=f"daily {component} ({component_unitsD[component]})")
-
+        plot_function(result["daily"]["time"], result["daily"][component], label=f"daily {component} ({component_unitsD[component]})")
         plot_data.append({
-            "x": [row.strftime("%Y-%m-%d") for row in data["daily"]["time"]],
-            "y": data["daily"][component],
+            "x": [row.strftime("%Y-%m-%d") for row in result["daily"]["time"]],
+            "y": result["daily"][component],
             "type": chart_type,
             "name": f"daily {component} ({component_unitsD[component]})"
         })
