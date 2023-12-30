204fa9ffed4b539419da68949d5e1465cd01e713
Võ Trường Dũng <87188466+mrrobjn@users.noreply.github.com>
2023-12-30 14:11:58 +0700

    fix

--- src/config/accountService.js
+++ src/config/accountService.js
@@ -1,20 +1,20 @@
-import admin from 'firebase-admin';
-import fs from "fs";
-import path from "path";
-import url from "url";
+// import admin from 'firebase-admin';
+// import fs from "fs";
+// import path from "path";
+// import url from "url";
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = url.fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(
-    path.resolve(__dirname, "../../serviceAccountKey.json"),
-    "utf8"
-  )
-);
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync(
+//     path.resolve(__dirname, "../../serviceAccountKey.json"),
+//     "utf8"
+//   )
+// );
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-export default admin;
+// export default admin;
